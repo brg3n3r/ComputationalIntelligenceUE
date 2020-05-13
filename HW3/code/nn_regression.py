1215e01c966eb121aac8290a41dd03608433ca9a
Christian <63261018+christobal550@users.noreply.github.com>
2020-05-13 13:14:43 +0200

    Final HW2 + Angabe HW3

---
+++ HW3/code/nn_regression.py
@@ -0,0 +1,102 @@
+import numpy as np
+from sklearn.metrics import mean_squared_error
+from sklearn.neural_network.multilayer_perceptron import MLPRegressor
+import matplotlib.pyplot as plt
+
+from nn_regression_plot import plot_mse_vs_neurons, plot_mse_vs_iterations, \
+    plot_learned_function, plot_mse_vs_alpha
+
+"""
+Assignment 3: Neural networks
+Part 1: Regression with neural networks
+
+This file contains functions to train and test the neural networks corresponding the the questions in the assignment,
+as mentioned in comments in the functions.
+Fill in all the sections containing TODOs.
+"""
+
+
+def calculate_mse(nn, x, y):
+    """
+    Calculates the mean squared error on the training and test data given the NN model used.
+    :param nn: An instance of MLPRegressor or MLPClassifier that has already been trained using fit
+    :param x: The data
+    :param y: The targets
+    :return: Training MSE, Testing MSE
+    """
+    ## TODO
+    mse = 0
+    return mse
+
+
+def ex_1_1_a(x_train, x_test, y_train, y_test):
+    """
+    Solution for exercise 1.1 a)
+    Remember to set alpha to 0 when initializing the model.
+    :param x_train: The training dataset
+    :param x_test: The testing dataset
+    :param y_train: The training targets
+    :param y_test: The testing targets
+    :return:
+    """
+
+    ## TODO
+    pass
+
+
+def ex_1_1_b(x_train, x_test, y_train, y_test):
+    """
+    Solution for exercise 1.1 b)
+    Remember to set alpha to 0 when initializing the model.
+    :param x_train: The training dataset
+    :param x_test: The testing dataset
+    :param y_train: The training targets
+    :param y_test: The testing targets
+    :return:
+    """
+
+    ## TODO
+    pass
+
+
+def ex_1_1_c(x_train, x_test, y_train, y_test):
+    """
+    Solution for exercise 1.1 c)
+    Remember to set alpha to 0 when initializing the model.
+    :param x_train: The training dataset
+    :param x_test: The testing dataset
+    :param y_train: The training targets
+    :param y_test: The testing targets
+    :return:
+    """
+
+    ## TODO
+    pass
+
+
+def ex_1_1_d(x_train, x_test, y_train, y_test):
+    """
+    Solution for exercise 1.1 d)
+    Remember to set alpha to 0 when initializing the model.
+    :param x_train: The training dataset
+    :param x_test: The testing dataset
+    :param y_train: The training targets
+    :param y_test: The testing targets
+    :return:
+    """
+
+    ## TODO
+    pass
+
+
+def ex_1_2(x_train, x_test, y_train, y_test):
+    """
+    Solution for exercise 1.2 a)
+    :param x_train: The training dataset
+    :param x_test: The testing dataset
+    :param y_train: The training targets
+    :param y_test: The testing targets
+    :return:
+    """
+    ## TODO
+    pass
